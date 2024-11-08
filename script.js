28c0058389659b2ef12c46a0f3445da8bb2e7aaa
Éderson <edersonramos952@gmail.com>
2024-11-07 21:03:45 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    const hamburguer = $(".hamburguer");
+    const nav = $("nav");
+
+    hamburguer.on("click", function() {
+        nav.slideToggle();
+    });      
+});
